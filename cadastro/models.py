fb164d965f8a3cd3753262d42b64041e5602b01a
ddavieiraalan <31701645+ddavieiraalan@users.noreply.github.com>
2021-05-06 16:28:44 -0300

    Criado o app Cadastro e enviado para o BD através do makemigrations

---
+++ cadastro/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=30)
+    cpf = models.CharField(min_length=11)
+
+    def __str__(self):
+        return self.nome
+    
