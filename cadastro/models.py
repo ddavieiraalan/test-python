c30e509d5fe74962371fcebe7ab6e52ed9a4b4e4
ddavieiraalan <31701645+ddavieiraalan@users.noreply.github.com>
2021-05-06 16:51:20 -0300

    Corrigido erro no dbsqlite3

--- cadastro/models.py
+++ cadastro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=30)
-    cpf = models.CharField(min_length=11)
+    cpf = models.CharField(max_length=11)
 
     def __str__(self):
         return self.nome
