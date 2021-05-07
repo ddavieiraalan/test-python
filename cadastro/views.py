59f75c54f6056361c4f3792ed52f471332f8bc01
ddavieiraalan <31701645+ddavieiraalan@users.noreply.github.com>
2021-05-07 15:12:15 -0300

    JWT Authentication implementado

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from cadastro.models import Cliente
 from cadastro.serializer import ClienteSerializer
 
 class ClientesViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from cadastro.views import ClientesViewSet
 from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 router = routers.DefaultRouter()
 router.register(r'clientes', ClientesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
